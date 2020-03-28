6ba4901a3f37846b71db0fd54225d65f9a3b4ee8
ShimizuShiori <116971263@qq.com>
2020-03-28 14:22:18 +0800

    引入 Dapper

---
+++ src/Reface.AppStarter.NPI/Reface.AppStarter.NPI/DapperSqlComandDescriptionExecutor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reface.AppStarter.Attributes;
+using Reface.NPI.Generators;
+using Dapper;
+
+namespace Reface.AppStarter.NPI
+{
+    [Component]
+    public class DapperSqlComandDescriptionExecutor : ISqlCommandDescriptionExecutor
+    {
+        public int Execute(DbConnectionContext dbConnectionContext, SqlCommandDescription sqlCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<object> Select(DbConnectionContext dbConnectionContext, SqlCommandDescription sqlCommand, Type entityType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
